554b3da40a6126b8255002c43a1d53d296bf60eb
Mindaugas Pikelis <mindaugas.pikelis@atea.lt>
2019-11-05 16:59:35 +0200

    start

---
+++ BlazorApp1/Data/Models.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    abstract class Animal
+    {
+        public string Name { get; set; }
+        public virtual string Color { get; set; }
+        public virtual List<string> Limbs { get; set; }
+    }
+
+    class Bird : Animal
+    {
+
+    }
+
+    class Dog : Animal
+    {
+
+    }
+
+    class Cat: Animal
+    {
+
+    }
+
+    public class Models
+    {
+        List<object> Animals = new List<object>();
+    }
+}
